52556e5bf0a747fabc418a2107edc341385a3cec
Brilliant Fitra Ramadhan <billyfitra35@gmail.com>
2024-06-16 01:06:23 +0700

    usenew

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ app.use(cors());
 app.use(express.json());
 
 // Koneksi ke MongoDB
-mongoose.connect(process.env.MONGODB_URI, {})
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.log('Failed to connect to MongoDB', err));
 
